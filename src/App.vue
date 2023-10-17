1f8efd93286b2c34201e1b8df87aca5967aaca8f
mercutio <mercutio_john@foxmail.com>
2023-10-17 13:24:40 +0800

    template init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+     <a href="https://vitejs.dev" target="_blank">
+       <img src="/vite.svg" class="logo" alt="Vite logo" />
+     </a>
+     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+       <img src="/javascript.svg" class="logo vanilla" alt="JavaScript logo" />
+     </a>
+     <h1>Hello Vite!</h1>
+     <div class="card">
+       <button type="button" @click="counter++">counter is {{counter}}</button>
+     </div>
+     <p class="read-the-docs">
+       Click on the Vite logo to learn more
+     </p>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name:"App",
+  components: {},
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import vue from '@vitejs/plugin-vue2'
+import { resolve } from 'path'
+
+export default {
+  plugins: [vue()],
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  }
+}
