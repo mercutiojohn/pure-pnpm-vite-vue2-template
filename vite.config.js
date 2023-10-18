fc6740fc67f1bc9e53b199eeb3082939f89b39e0
mercutio <mercutio_john@foxmail.com>
2023-10-18 11:09:54 +0800

    add jsx support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import vue from '@vitejs/plugin-vue2'
+import vueJsx from '@vitejs/plugin-vue2-jsx'
 import { resolve } from 'path'
 
 export default {
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-preset-jsx
+    })
+  ],
 
   resolve: {
     alias: {
